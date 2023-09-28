e59d19f531586362bda45dd357259919c6898d25
nathy10carvalh0 <nathy10carvalho@gmail.com>
2023-09-28 15:20:25 -0300

    Convertendo o modelo do PokeApi para nosso modelo

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,13 +1,30 @@
 
 const pokeapi = {}
 
+function convertPokeApiDetailToPokemon(pokeDetail){
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.order
+    pokemon.name = pokeDetail.name
+
+    const types =  pokeDetail.types.map((typeSlot)=> typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+
 pokeapi.getPokemonsDetail = (pokemon) => {
     return  fetch(pokemon.url)
         .then((response)=> response.json())
+        .then(convertPokeApiDetailToPokemon)
 }
 
 
-pokeapi.getPokemons = (offset = 0, limit = 10)  => {
+pokeapi.getPokemons = (offset = 0, limit = 5)  => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
     return fetch(url)
         .then((response)=> response.json()) 
