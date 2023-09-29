c5fe2ced7d3a82aec753a15475d5459cc06606ea
nathy10carvalh0 <nathy10carvalho@gmail.com>
2023-09-29 12:16:15 -0300

    adicionando botão para pagina de detalhes

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -3,7 +3,7 @@ const pokeapi = {}
 
 function convertPokeApiDetailToPokemon(pokeDetail){
     const pokemon = new Pokemon()
-    pokemon.number = pokeDetail.order
+    pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
 
     const types =  pokeDetail.types.map((typeSlot)=> typeSlot.type.name)
@@ -13,6 +13,16 @@ function convertPokeApiDetailToPokemon(pokeDetail){
     pokemon.type = type
 
     pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+    pokemon.photoShiny = pokeDetail.sprites.other.dream_world.front_shiny
+
+    const abilities = pokeDetail.abilities.map((slot) => {
+        let str = slot.ability.name
+        return str.charAt(0).toUpperCase() + str.slice(1)
+    }).join(', ')
+    pokemon.abilities = abilities
+
+    pokemon.height = pokeDetail.height
+    pokemon.weight = pokeDetail.weight
 
     return pokemon
 }
@@ -33,3 +43,10 @@ pokeapi.getPokemons = (offset = 0, limit = 6)  => {
         .then((detailRequests)=> Promise.all(detailRequests))
         .then((pokemonDetails)=> pokemonDetails)
 }
+pokeapi.getPokemonDetailById = (id) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+
+    return fetch(url)
+        .then((response) => response.json())
+        .then((pokemon) => convertPokeApiDetailToPokemon(pokemon))
+}   
